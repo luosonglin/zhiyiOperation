6101873c5c7404f95b4a6491debd3855b0dd6495
luosonglin <luo1992athrun1102@qq.com>
2016-12-29 11:28:55 +0800

    init operation backend project

---
+++ src/main/java/com/medmeeting/ZhiyiOperaionApplication.java
@@ -0,0 +1,12 @@
+package com.medmeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhiyiOperaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhiyiOperaionApplication.class, args);
+	}
+}
---
+++ src/main/java/com/medmeeting/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.medmeeting.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by luosonglin on 29/12/2016.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
